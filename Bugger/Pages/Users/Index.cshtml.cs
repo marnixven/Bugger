673e33177c61b5273be673010f29f1bba3312857
Marnix Ven <marnixven@hotmail.com>
2021-01-02 00:58:04 +0100

    Fetched data on Users page

--- Bugger/Pages/Users/Index.cshtml.cs
+++ Bugger/Pages/Users/Index.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Bugger.Models;
+using Bugger.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,18 @@ namespace Bugger.Pages.Users
 {
     public class IndexModel : PageModel
     {
+        private readonly IUserRepository userRepository;
+
+        public IEnumerable<User> Users { get; set; }
+
+        public IndexModel(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+        
         public void OnGet()
         {
+            Users = userRepository.GetAllUsers();
         }
     }
 }
--- Bugger/Startup.cs
+++ Bugger/Startup.cs
@@ -1,3 +1,4 @@
+using Bugger.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -24,6 +25,7 @@ namespace Bugger
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddSingleton<IUserRepository, MockUserRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
