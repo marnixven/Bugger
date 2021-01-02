9f0c133294f16e9f4adcf62d27181b22e8c777ec
Marnix Ven <marnixven@hotmail.com>
2021-01-02 11:24:58 +0100

    Added Bugger.Models/Projects

---
+++ Bugger.Models/Projects.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bugger.Models
+{
+    public class Project
+    {
+        public int ProjectId { get; set; }
+        public string ProjectName { get; set; }
+        public string CreatedBy { get; set; }
+        public string AssignedToUserId { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime TargetEndDate { get; set; }
+        public DateTime ActualEndDate { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+}
