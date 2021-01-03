882cc150df2cb777881bed7d71e37769a01eb4ae
Marnix Ven <marnixven@hotmail.com>
2021-01-03 17:33:48 +0100

    Set LowercaseUrls to "true"

--- Bugger/Startup.cs
+++ Bugger/Startup.cs
@@ -2,6 +2,7 @@ using Bugger.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,6 +28,12 @@ namespace Bugger
             services.AddRazorPages();
             services.AddSingleton<IUserRepository, MockUserRepository>();
             services.AddSingleton<IProjectRepository, MockProjectRepository>();
+            services.Configure<RouteOptions>(options =>
+            {
+                options.LowercaseUrls = true;
+                options.LowercaseQueryStrings = true;
+                options.AppendTrailingSlash = true;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
